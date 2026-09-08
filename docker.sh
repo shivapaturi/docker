31d97dc0e746ba1dae447d015b243d0cb4f26908
shiva paturi <shivapaturi01@gmail.com>
2026-09-08 21:32:44 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
 
 growpart /dev/nvme0n1 4
 lvextend -L +20G /dev/RootVG/rootVol
