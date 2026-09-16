924e9212adab58e45b7498d43742e88c432d47ae
shiva paturi <shivapaturi01@gmail.com>
2026-09-16 20:13:55 +0530

    kubernetes

--- docker.sh
+++ docker.sh
@@ -1,31 +1,74 @@
 #!/bin/bash
 
+# Docker
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo systemctl start docker
-sudo systemctl enable docker
+
+sudo dnf config-manager --add-repo \
+  https://download.docker.com/linux/rhel/docker-ce.repo
+
+sudo dnf install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+sudo systemctl enable --now docker
+
 sudo usermod -aG docker ec2-user
 
-growpart /dev/nvme0n1 4
-lvextend -L +20G /dev/RootVG/rootVol
-lvextend -L +10G /dev/RootVG/varVol
 
-xfs_growfs /
-xfs_growfs /var
+# Storage
+sudo growpart /dev/nvme0n1 4
 
+sudo lvextend -L +20G /dev/RootVG/rootVol
+sudo lvextend -L +10G /dev/RootVG/varVol
+
+sudo xfs_growfs /
+sudo xfs_growfs /var
+
+
+# eksctl
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
+curl -sLO \
+  "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
 
+tar -xzf "eksctl_${PLATFORM}.tar.gz" -C /tmp
+rm -f "eksctl_${PLATFORM}.tar.gz"
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin/eksctl
+rm -f /tmp/eksctl
+
+
+# kubectl
+curl -LO \
+  https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+
+# Git
+sudo dnf install -y git
+
+
+# kubens / kubectx
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+
+sudo ln -sf /opt/kubectx/kubens /usr/local/bin/kubens
+sudo ln -sf /opt/kubectx/kubectx /usr/local/bin/kubectx
+
+
+# Verify
+echo "Docker:"
+docker --version
+
+echo "eksctl:"
 eksctl version
-kubectl version
 
-git clone https://github.com/ahmetb/kubectx /opt/kubectx
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+echo "kubectl:"
+kubectl version --client
+
+echo "Installation completed."
