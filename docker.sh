adab790595c94c7fab3d168ba4c4a62d83771465
shiva paturi <shivapaturi01@gmail.com>
2026-09-21 21:03:26 +0530

    kubernetes

--- docker.sh
+++ docker.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
 
-# Docker
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
 
-sudo dnf -y install dnf-plugins-core
-
-sudo dnf config-manager --add-repo \
-  https://download.docker.com/linux/rhel/docker-ce.repo
-
-sudo dnf install -y \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-compose-plugin
-
-sudo systemctl enable --now docker
-
-sudo usermod -aG docker ec2-user
-
-# Storage
-
-sudo growpart /dev/nvme0n1 4
-
-sudo lvextend -L +20G /dev/RootVG/rootVol
-sudo lvextend -L +10G /dev/RootVG/varVol
-
-sudo xfs_growfs /
-sudo xfs_growfs /var
+xfs_growfs /
+xfs_growfs /var
 
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
